9477736be262757b05d93db7515d5b2758182103
nedaei79 <nedaei79@gmail.com>
2020-05-02 01:16:16 +0430

    buyer submenus finalized.

--- src/main/java/view/nedaei/buyermenu/PurchasePanel.java
+++ src/main/java/view/nedaei/buyermenu/PurchasePanel.java
@@ -1,6 +1,11 @@
 package view.nedaei.buyermenu;
 
+import controller.Controller;
+import view.bagheri.Login_RegisterPanel;
 import view.bagheri.Panel;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class PurchasePanel extends Panel {
     private static PurchasePanel instance;
@@ -18,6 +23,9 @@ public class PurchasePanel extends Panel {
 
     @Override
     protected void execute() {
+        if (!Controller.getInstance().isLoggedIn()) {
+            Login_RegisterPanel.getInstance().execute();
+        }
         runReceiverInformationPanel();
         runDiscountCodePanel();
         runPaymentPanel();
@@ -28,7 +36,13 @@ public class PurchasePanel extends Panel {
 
             @Override
             public void execute() {
-
+                ArrayList<String> buyLogInformationToReceive = Controller.getInstance().getBuyLogInformationToReceive());
+                HashMap<String, String> fieldsAndValues = new HashMap<String, String>();
+                for (String field : buyLogInformationToReceive) {
+                    System.out.println(field + ":");
+                    fieldsAndValues.put(field, scanner.nextLine().trim());
+                }
+                Controller.getInstance().createBuyLog(fieldsAndValues);
             }
 
         }.execute();
@@ -39,7 +53,21 @@ public class PurchasePanel extends Panel {
 
             @Override
             public void execute() {
-                super.execute();
+                System.out.println("would you like to use any discount codes?");
+                String input;
+                while (!(input = scanner.nextLine().trim()).equalsIgnoreCase("no")) {
+                    if (input.equalsIgnoreCase("yes")) {
+                        System.out.println("discount code:");
+                        if (Controller.getInstance().isDiscountCodeValid(input = scanner.nextLine().trim())) {
+                            Controller.getInstance().applyDiscountCode(input);
+                        } else {
+                            System.out.println("invalid discount code!");
+                        }
+                        return;
+                    } else {
+                        System.out.println("invalid command!");
+                    }
+                }
             }
 
         }.execute();
@@ -50,7 +78,11 @@ public class PurchasePanel extends Panel {
 
             @Override
             protected void execute() {
-                super.execute();
+                if (!Controller.getInstance().canPurchase()) {
+                    System.out.println("you do not have enough balance!");
+                    return;
+                }
+                Controller.getInstance().purchase();
             }
 
         }.execute();

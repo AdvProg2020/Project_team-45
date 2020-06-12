51622381eed90753f4db0f033dc369fff851664b
smmhatami <hatamik7@gmail.com>
2020-06-12 23:16:55 +0430

    ProductsManagingMenuForAdmin modified (not tested)

--- src/main/java/newViewHatami/ProductsManagingMenuForAdmin.java
+++ src/main/java/newViewHatami/ProductsManagingMenuForAdmin.java
@@ -1,8 +1,56 @@
 package newViewHatami;
 
+import controller.ProductController;
+import graphicview.nedaei.MenuController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import model.Market;
+import model.product.Product;
+
+import java.util.ArrayList;
+
 public class ProductsManagingMenuForAdmin extends AppMenu {
+    public ListView productsList;
+    public Label errorLabel;
+
+    @FXML
+    public void initialize(){
+        fillList();
+    }
+
+    private void fillList() {
+        ArrayList<Product> allProductsList = Market.getInstance().getAllProducts();
+        ObservableList<String> items = FXCollections.observableArrayList();
+        for (Product product : allProductsList) {
+            items.add(product.getId());
+        }
+        productsList.setItems(items);
+    }
+
     public static String getFxmlFilePath() {
         return "/ProductsManagingMenuForAdmin.fxml";
     }
 
+    public void viewSelectedProduct() {
+        // TODO
+    }
+
+    public void deleteSelectedProduct() {
+        String selectedProductId = getSelectedProductId();
+        ProductController.getInstance().deleteItemById(selectedProductId);
+        fillList();
+        errorLabel.setText("product removed successfully!");
+
+    }
+
+    public String getSelectedProductId(){
+        return (String) productsList.getSelectionModel().getSelectedItem();
+    }
+
+    public void back() {
+        MenuController.getInstance().goToMenu(AdminMenu.getFxmlFilePath());
+    }
 }

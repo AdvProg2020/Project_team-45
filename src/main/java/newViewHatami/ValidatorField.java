9595c4d302df3163d4f2924a0c53839cd62dd4f4
smmhatami <hatamik7@gmail.com>
2020-06-14 01:53:30 +0430

    ValidatorField modified 2, not finished

--- src/main/java/newViewHatami/ValidatorField.java
+++ src/main/java/newViewHatami/ValidatorField.java
@@ -1,6 +1,8 @@
 package newViewHatami;
 
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
@@ -10,21 +12,30 @@ public class ValidatorField extends TextField {
 
     private String validatorRegex;
 
-    private final StringProperty validation = new SimpleStringProperty(this, "fieldType", ".*");
+    private final StringProperty validation = new SimpleStringProperty(this, "fieldType", "none");
 //    public final StringProperty fieldTypeProperty() { return fieldType; }
     public final String getValidation() { return validation.get(); }
     public final void setValidation(String value) { validation.set(value); }
 
-    private final StringProperty isNecessary = new SimpleStringProperty(this, "fieldType", ".*");
+    private final BooleanProperty isNecessary = new SimpleBooleanProperty(this, "fieldType", false);
     //    public final StringProperty fieldTypeProperty() { return fieldType; }
-    public final String getIsNecessary() { return isNecessary.get(); }
-    public final void setIsNecessary(String value) { isNecessary.set(value); }
+    public final boolean getIsNecessary() { return isNecessary.get(); }
+    public final void setIsNecessary(boolean value) { isNecessary.set(value); }
 
     @FXML
     public boolean validate() {
-        if (validatorRegex == null) {
-            if (!setValidatorRegex())
+        if (getText().equals("")) {
+            if (isNecessary.get()) {
+                this.setStyle("-fx-text-box-border: #fc0606;");
+                return false;
+            }
+            else {
+                this.setStyle("-fx-text-box-border: #039ed3;");
                 return true;
+            }
+        }
+        if (validatorRegex == null) {
+            setValidatorRegex();
         }
         String value = getText();
         if (value.matches(validatorRegex)) {
@@ -36,21 +47,21 @@ public class ValidatorField extends TextField {
         return false;
     }
 
-    private boolean setValidatorRegex() {
-        if (getValidation() == null)
-            return false;
+    private void setValidatorRegex() {
         switch (getValidation()) {
+            case "none":
+                validatorRegex = ".*";
+                break;
             case "alphaNumeric":
-                validatorRegex = "\\w+";
+                validatorRegex = "\\w*";
                 break;
             case "englishTense":
-                validatorRegex = "[a-zA-Z ]+";
+                validatorRegex = "[a-zA-Z ]*";
                 break;
             case "emailAddress":
                 validatorRegex = "\\w+@\\w+.\\w+";
                 break;
         }
-        return true;
     }
 
 }

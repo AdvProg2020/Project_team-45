498eb0534f5c365e89a965d78b1926595c5b5800
nedaei79 <nedaei79@gmail.com>
2020-06-15 18:14:17 +0430

    seller -> show categories panel (copied from hatam!)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         Market.getInstance().initialize();
-        UserController.getInstance().setActiveUserByUsername("b");
+        UserController.getInstance().setActiveUserByUsername("s");
         UserController.setLoggedIn(true);
         launch(args);
     }
--- src/main/java/newViewNedaei/user/seller/ShowCategoriesPanel.java
+++ src/main/java/newViewNedaei/user/seller/ShowCategoriesPanel.java
@@ -1,12 +1,18 @@
 package newViewNedaei.user.seller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import model.Market;
+import model.category.Category;
+import model.category.ParentCategory;
 import newViewNedaei.MenuController;
+
+import java.util.ArrayList;
 
 public class ShowCategoriesPanel {
     @FXML
-    private TreeView treeView;
+    private TreeView<String> treeView;
 
     public static String getFxmlFilePath() {
         return "/ShowCategoriesPanel.fxml";
@@ -14,5 +20,29 @@ public class ShowCategoriesPanel {
 
     public void goBack() {
         MenuController.getInstance().enableCurrentPane();
+    }
+
+    @FXML
+    public void initialize() {
+        ArrayList<Category> mainCategories = Market.getInstance().getMainCategories();
+        TreeItem<String> rootNode = new TreeItem<>("categories");
+        for (Category mainCategory : mainCategories) {
+            TreeItem<String> mainNode = new TreeItem<>(mainCategory.getName());
+            addChildrenToNode(mainNode, mainCategory);
+            rootNode.getChildren().add(mainNode);
+        }
+        treeView.setRoot(rootNode);
+    }
+
+    private void addChildrenToNode(TreeItem<String> rootNode, Category category) {
+        if (category.isFinal())
+            return;
+        rootNode.setExpanded(true);
+        ArrayList<Category> subCategories = ((ParentCategory) category).getSubcategories();
+        for (Category subCategory : subCategories) {
+            TreeItem<String> node = new TreeItem<>(subCategory.getName());
+            addChildrenToNode(node, subCategory);
+            rootNode.getChildren().add(node);
+        }
     }
 }

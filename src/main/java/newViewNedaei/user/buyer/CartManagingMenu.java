bb40cbe50a6e2189fc33088810edb394e4f607c2
nedaei79 <nedaei79@gmail.com>
2020-06-15 11:55:58 +0430

    buyer finalized except for cart.

--- src/main/java/newViewNedaei/user/buyer/CartManagingMenu.java
+++ src/main/java/newViewNedaei/user/buyer/CartManagingMenu.java
@@ -1,5 +1,15 @@
 package newViewNedaei.user.buyer;
 
+import javafx.scene.input.KeyEvent;
+import newViewHatami.Validator;
+
 public class CartManagingMenu {
 
+    public static String getFxmlFilePath() {
+        return "/CartManagingMenu.fxml";
+    }
+
+    public void validate(KeyEvent keyEvent) {
+        ((Validator) keyEvent.getSource()).validate();
+    }
 }

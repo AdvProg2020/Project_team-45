d764fe80ae0c1e0cf467b80f90942c51d63b4138
nedaei79 <nedaei79@gmail.com>
2020-07-15 12:32:54 +0430

    primary database methods and new packages.

---
+++ src/main/java/DatabaseMain.java
@@ -0,0 +1,10 @@
+import newController.DatabaseController;
+
+public class DatabaseMain {
+    public static void main(String[] args) {
+        DatabaseController databaseController = new DatabaseController();
+//        databaseController.createUsersTable();
+        databaseController.writeAnExampleInDatabase();
+        databaseController.closeDataBase();
+    }
+}
---
+++ src/main/java/newModel/Example.java
@@ -0,0 +1,4 @@
+package newModel;
+
+public class Example {
+}

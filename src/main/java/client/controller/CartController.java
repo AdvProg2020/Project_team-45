00da1929480b617046b6ff5946a2366e0d623f10
nedaei79 <nedaei79@gmail.com>
2020-07-22 13:21:02 +0430

    cart controller.(server and client)

--- src/main/java/client/controller/CartController.java
+++ src/main/java/client/controller/CartController.java
@@ -1,13 +1,6 @@
 package client.controller;
 
-import client.controller.userControllers.BuyerController;
-import client.network.ClientSocket;
 import client.network.MethodStringer;
-import server.model.product.Product;
-import server.model.product.ProductSellInfo;
-import server.model.user.CartHolder;
-
-import java.lang.reflect.Method;
 
 public class CartController {
     private static final CartController instance = new CartController();
@@ -26,14 +19,4 @@ public class CartController {
             throwable.printStackTrace();
         }
     }
-
-    public void addProductToCart(Product product, ProductSellInfo productSellInfo) {
-        BuyerController.getInstance().updateBuyer();
-        CartHolder buyer = BuyerController.getInstance().getBuyer();
-        buyer.getCart().addProduct(product, productSellInfo);
-    }
-
-    public void changeCartProductAmount(CartHolder cartHolder, String productId, String increaseOrDecrease) {
-    }
-
 }
